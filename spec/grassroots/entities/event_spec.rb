5ed19736b56c36e9a125989acb377be391293f05
kamalogudah <mcpaul2058@gmail.com>
2018-09-29 21:33:33 +0300

    Working with entities

--- spec/grassroots/entities/event_spec.rb
+++ spec/grassroots/entities/event_spec.rb
@@ -1,5 +1,8 @@
 require_relative '../../spec_helper'
 
 describe Event do
-  # place your tests here
+  # it 'can be initialized with attributes' do
+  #   event = Event.new(title: 'Gor Mahia vs Afc Leopards', description: 'Good game', location: 'Kasarani', start_time: Time.now, end_time: Time.now)
+  #   event.description.must_equal 'Gor Mahia vs Afc Leopards'
+  # end
 end
